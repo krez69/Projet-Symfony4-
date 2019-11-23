d87af2945a353d479fac76553c0ecc2ebe1c867c
krez69 <hamimed.walid@gmail.com>
2019-11-23 17:14:22 +0100

    create routing show

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -17,4 +17,18 @@ class WildController extends AbstractController
     {
         return $this->render('wild/index.html.twig', ['website' => 'Wild Series',]);
     }
+
+    /**
+     * @Route("/wild/show/{slug}", name="wild_show", requirements={"slug"="[a-z0-9\-]*"})
+     * @param $slug
+     * @return Response
+     */
+    public function show($slug): Response
+    {
+        if ($slug ==='') {
+            return $this->render('wild/show.html.twig', ['slug' => $slug = "Aucune série sélectionnée, veuillez choisir une série"]);
+        }
+
+        return $this->render('wild/show.html.twig', ['slug' => $slug = ucwords(str_replace('-', ' ', $slug))]);
+    }
 }
